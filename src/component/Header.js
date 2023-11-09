1697657bfa02b1910b5ddee8c3ee1427c9c32ba4
Jesse Granier <jesse.granier@lapiscine.pro>
2023-11-09 16:14:23 +0100

    Link

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,4 +1,5 @@
 import ProfilMenu from './ProfilMenu';
+import { Link } from "react-router-dom";
 
 
 function Header() {
@@ -14,9 +15,10 @@ function Header() {
         <nav className='navHeader01'>
             <ul className='ulHeader01'>
             <ProfilMenu />
-                <li className='liHeader01'>Accueil</li>
-                <li className='liHeader01'>Home</li>                
-                <li className='liHeader01'>Contact</li> 
+                <li>Accueil</li>
+                <li><Link to="/">Home</Link></li>                
+                <li><Link to="/contact">Contact</Link></li> 
+                <li><Link to="/story">Story</Link></li>
             </ul>
         </nav>
         </header>
